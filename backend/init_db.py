799717b96df8f1ccb756a00bf8cfc7a7677306e9
George Pîrlea <george@dranov.ro>
2014-02-05 16:56:32 +0200

    Fixed off-by-one in init_db.py; configurable number of fakes

--- backend/init_db.py
+++ backend/init_db.py
@@ -3,6 +3,9 @@
 import oxp.storage
 import faker, random
 
+NUM_FAKE_CATEGORIES = 10
+NUM_FAKE_EXPENSES = 100
+
 oxp.storage.db.connect()
 oxp.storage.Expense.create_table()
 oxp.storage.Category.create_table()
@@ -10,16 +13,16 @@ oxp.storage.Category.create_table()
 fake = faker.Faker()
 
 # Add fake categories
-for cat in range(10):
+for cat in range(NUM_FAKE_CATEGORIES):
     cat = oxp.storage.Category(name=fake.bs())
     cat.save()
 
 
 # Add fake expenses
-for exp in range(random.randint(3, 15)):
+for exp in range(NUM_FAKE_EXPENSES):
     exp = oxp.storage.Expense(name=fake.word(),
             sum=fake.random_number(digits=4),
-            category_id=random.randrange(10))
+            category_id=random.randint(1, NUM_FAKE_CATEGORIES))
     exp.save()
 
 
