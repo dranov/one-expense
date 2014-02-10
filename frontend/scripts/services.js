b5a4e193233a0f4bdbc0d5850eaee3514101f472
Marcu Iulian <marcu.iulian13@gmail.com>
2014-02-10 17:32:02 +0200

    Made categories adding modal prettier.

--- frontend/scripts/services.js
+++ frontend/scripts/services.js
@@ -1,9 +1,9 @@
 var services = angular.module('services', ['ngResource']);
 
 services.factory('Categories', function ($resource) {
-    return $resource('http://localhost:5000/api/categories', {});
+    return $resource('http://192.168.0.102:5000/api/categories', {});
 });
 
 services.factory('Category', function ($resource) {
-    return $resource('http://localhost:5000/api/category', {}, {});
+    return $resource('http://192.168.0.102:5000/api/category', {}, {});
 });
